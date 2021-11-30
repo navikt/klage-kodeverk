ac835f4d958cb95225bd7f20b5837294abbdc7f4
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-11-30 10:04:04 +0100

    Adjusted build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.0"
 	kotlin("plugin.spring") version "1.6.0"
+	id("maven-publish")
 }
 
 group = "no.nav.klage"
