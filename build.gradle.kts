e1352e11bbb3b04ef0642e06a0179f39d9c45297
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-11-30 13:37:55 +0100

    Removed Spring.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.1"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.0"
-	kotlin("plugin.spring") version "1.6.0"
 	id("maven-publish")
+	id("java-library")
 }
 
 group = "no.nav.klage"
 version = properties["version"] ?: "local-build"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("jakarta.persistence:jakarta.persistence-api:2.2.3")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+	testImplementation("org.assertj:assertj-core:3.11.1")
 }
 
 tasks.withType<KotlinCompile> {
