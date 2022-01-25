0ba71dc86113cc8a9a811db31faac5c5419ef643
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-01-25 19:55:48 +0100

    Added missing enhet.

--- src/main/kotlin/no/nav/klage/kodeverk/Enhet.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/Enhet.kt
@@ -444,7 +444,9 @@ enum class Enhet(override val id: String, override val navn: String, override va
     E2990("529", "2990", "IT-avdelingen"),
     E4425("530", "4425", "NAV Arbeid og ytelser lønnsgaranti"),
     E4426("531", "4426", "NAV Arbeid og ytelser lønnsgaranti Vardø"),
-    E1783("532", "1783", "NAV Værnes")
+    E1783("532", "1783", "NAV Værnes"),
+    E2830("533", "2830", "DIR Ytelsesavdelingen")
+
 }
 
 val klageenheter = setOf(Enhet.E4295, Enhet.E4293, Enhet.E4294, Enhet.E4292, Enhet.E4291, Enhet.E4250, Enhet.E2103)
