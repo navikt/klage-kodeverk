46e2f6088224d3e6239483f9768c07de585a6920
Andreas Jonsson <andreas.jonsson@nav.no>
2022-01-31 12:09:29 +0100

    Lagt til manglende hjemmel for SUP_UFF

--- src/main/kotlin/no/nav/klage/kodeverk/hjemmel/YtelseTilRegistreringshjemler.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/hjemmel/YtelseTilRegistreringshjemler.kt
@@ -2389,6 +2389,7 @@ val ytelseTilRegistreringshjemler = mapOf(
         SUP_ST_L_8,
         SUP_ST_L_9,
         SUP_ST_L_10,
+        SUP_ST_L_11,
         SUP_ST_L_12,
         SUP_ST_L_13,
         SUP_ST_L_17,
