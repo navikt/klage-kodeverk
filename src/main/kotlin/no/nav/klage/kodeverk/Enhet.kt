7c5e187b3fb55307ef00babb4ddebca2d53ccb13
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-04-04 08:47:50 +0200

    La til styringsenhet 4400.

--- src/main/kotlin/no/nav/klage/kodeverk/Enhet.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/Enhet.kt
@@ -451,8 +451,9 @@ enum class Enhet(override val id: String, override val navn: String, override va
     E4151("535", "4151", "NAV Økonomi Stønad"),
     E5702("537", "5702", "NAV Lerkendal"),
     E5701("538", "5701", "NAV Falkenborg"),
+    E4400("539", "4400", "NAV Arbeid og ytelser styringsenhet"),
 }
 
 val klageenheter = setOf(Enhet.E4295, Enhet.E4293, Enhet.E4294, Enhet.E4292, Enhet.E4291, Enhet.E4250, Enhet.E2103)
 
-val styringsenheter = setOf(Enhet.E4200)
+val styringsenheter = setOf(Enhet.E4200, Enhet.E4400)
