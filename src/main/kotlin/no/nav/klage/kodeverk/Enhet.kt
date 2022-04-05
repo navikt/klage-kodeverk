dc006a64f56d5d4948aa186624b1e972c8e4875d
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-04-05 15:11:54 +0200

    La til enhet 4819.

--- src/main/kotlin/no/nav/klage/kodeverk/Enhet.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/Enhet.kt
@@ -453,6 +453,7 @@ enum class Enhet(override val id: String, override val navn: String, override va
     E5701("538", "5701", "NAV Falkenborg"),
     E4400("539", "4400", "NAV Arbeid og ytelser styringsenhet"),
     E0335("540", "0335", "NAV Ullern"),
+    E4819("541", "4819", "NAV Økonomi Pensjon")
 }
 
 val klageenheter = setOf(Enhet.E4295, Enhet.E4293, Enhet.E4294, Enhet.E4292, Enhet.E4291, Enhet.E4250, Enhet.E2103)
