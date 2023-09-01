e7a0eb2234d437ff75df3b0b176f9abc7d4df5df
Andreas Jonsson <andreas.jonsson@nav.no>
2023-09-01 10:21:46 +0200

    Moved Templates (types of smartdocuments) to BE

---
+++ src/main/kotlin/no/nav/klage/kodeverk/Template.kt
@@ -0,0 +1,28 @@
+package no.nav.klage.kodeverk
+
+enum class Template(override val id: String, override val navn: String, override val beskrivelse: String) : Kode {
+
+    NOTAT("notat", "notat", "Notat"),
+    GENERELT_BREV("generelt_brev", "Generelt brev", "Generelt brev"),
+    ANKEVEDTAK("ankevedtak", "ankevedtak", "Ankevedtak"),
+    KLAGEVEDTAK("klagevedtak", "klagevedtak", "Klagevedtak"),
+    ROL_NOTAT("rol-notat", "rol-notat", "ROL-notat"),
+    ;
+
+    override fun toString(): String {
+        return "Template(id=$id, " +
+                "navn=$navn)"
+    }
+
+    companion object {
+        fun of(id: String): Template {
+            return entries.firstOrNull { it.id == id }
+                ?: throw IllegalArgumentException("No Template with id $id exists")
+        }
+
+        fun fromNavn(navn: String): Template {
+            return entries.firstOrNull { it.navn == navn }
+                ?: throw IllegalArgumentException("No Template with navn $navn exists")
+        }
+    }
+}
