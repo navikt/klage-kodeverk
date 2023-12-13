71d6b87882cb14b6ecb6be6aee97f1bd46981192
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2023-12-13 14:43:49 +0100

    Specify Kjennelse fra Trygderetten

--- src/main/kotlin/no/nav/klage/kodeverk/DokumentType.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/DokumentType.kt
@@ -17,7 +17,7 @@ enum class DokumentType(
     //VEDLEGG("3", "Vedlegg", "Vedlegg", "vedlegg.pdf"),
     VEDTAK("4", "Vedtak", "Vedtaksbrev fra Klageinstans", "vedtaksbrev.pdf"),
     BESLUTNING("5", "Beslutning", "Beslutningsbrev fra Klageinstans", "beslutningsbrev.pdf"),
-    INNGAAENDE("6", "Inngående dokument", "Inngående dokument til Klageinstans", "inngaaende.pdf"),
+    KJENNELSE_FRA_TRYGDERETTEN("6", "Kjennelse fra Trygderetten", "Inngående kjennelse fra Trygderetten", "Kjennelse fra Trygderetten.pdf"),
     ;
 
     override fun toString(): String {
