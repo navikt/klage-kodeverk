8e190701a901e7de9467256638be5cee2cec25cd
Andreas Jonsson <andreas.jonsson@nav.no>
2025-08-13 09:31:18 +0200

    Updated text.

--- src/main/kotlin/no/nav/klage/kodeverk/SattPaaVentReason.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/SattPaaVentReason.kt
@@ -5,7 +5,7 @@ enum class SattPaaVentReason(override val id: String, override val navn: String,
     VENTER_PAA_TILSVAR("1", "VENTER_PAA_TILSVAR", "Venter på tilsvar"),
     VENTER_PAA_DOKUMENTASJON("2", "VENTER_PAA_DOKUMENTASJON", "Venter på dokumentasjon"),
     VENTER_PAA_AVKLARING_OM_DOEDSBO("3", "VENTER_PAA_AVKLARING_OM_DOEDSBO", "Venter på avklaring om dødsbo"),
-    SATT_I_BERO("4", "SATT_I_BERO", "Satt i bero"),
+    SATT_I_BERO("4", "SATT_I_BERO", "Satt i bero etter instruks"),
     ANNET("5", "ANNET", "Annet"),
     ;
 
