c2defce723462159a7854b8f07d10f35d2721171
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-09-01 21:45:23 +0200

    WIP begjæring om gjenopptak.

--- src/main/kotlin/no/nav/klage/kodeverk/Utfall.kt
+++ src/main/kotlin/no/nav/klage/kodeverk/Utfall.kt
@@ -19,6 +19,14 @@ enum class Utfall(override val id: String, override val navn: String, override v
     MEDHOLD_ETTER_FVL_35("13", "Medhold etter forvaltningsloven § 35", "Medhold etter forvaltningsloven § 35"),
     BESLUTNING_IKKE_OMGJOERE("14", "Beslutning om ikke å omgjøre", "Beslutning om ikke å omgjøre"),
     STADFESTET_ANNEN_BEGRUNNELSE("15", "Stadfestet med en annen begrunnelse", "Stadfestet med en annen begrunnelse"),
+    INNSTILLING_BEGJAERING_TAS_TIL_FOELGE_VEDTAK_OPPHEVES("16", "Innstilling: Begjæringen tas til følge - KAs vedtak oppheves", "Innstilling: Begjæringen tas til følge - KAs vedtak oppheves"),
+    INNSTILLING_BEGJAERING_TAS_TIL_FOELGE_VEDTAK_STADFESTES("17", "Innstilling: Begjæringen tas til følge - KAs vedtak stadfestes", "Innstilling: Begjæringen tas til følge - KAs vedtak stadfestes"),
+    INNSTILLING_BEGJAERING_TAS_IKKE_TIL_FOELGE("18", "Innstilling: Begjæringen tas ikke til følge", "Innstilling: Begjæringen tas ikke til følge"),
+    BEGJAERING_TAS_IKKE_TIL_FOELGE("19", "Begjæringen tas ikke til følge", "Begjæringen tas ikke til følge"),
+    BEGJAERING_AVVISES("20", "Begjæringen avvises", "Begjæringen avvises"),
+    BEGJAERING_TAS_TIL_FOELGE_VEDTAK_OPPHEVES("21", "Begjæringen tas til følge - KAs vedtak oppheves", "Begjæringen tas til følge - KAs vedtak oppheves"),
+    BEGJAERING_TAS_TIL_FOELGE_VEDTAK_STADFESTES("22", "Begjæringen tas til følge - KAs vedtak stadfestes", "Begjæringen tas til følge - KAs vedtak stadfestes"),
+    BEGJAERING_TAS_TIL_FOELGE_DELVIS_ELLER_FULLT_MEDHOLD("23", "Begjæringen tas til følge - Delvis eller fullt medhold", "Begjæringen tas til følge - Delvis eller fullt medhold"),
     ;
 
 
